323bf8ce5fd738a5f841848ac922f1694572ebd5
Guillaume Roullet <roullet@univ-brest.fr>
2024-11-28 07:50:43 +0100

    Set #threads equal to the number of cores

--- src/fluids2d/parallel.py
+++ src/fluids2d/parallel.py
@@ -1,14 +1,16 @@
-from numba import jit, prange, set_num_threads
+from numba import jit, prange, set_num_threads, config
 
-set_num_threads(12)
+_nthreads = config.NUMBA_DEFAULT_NUM_THREADS
+set_num_threads(_nthreads)
 
-@jit(parallel=True)
+
+@ jit(parallel=True)
 def thread_execution(func, arrays, args, nthreads, halo):
     N = arrays[0].size
     n = N//nthreads
     for i in prange(nthreads):
-        #idx = slice(max(0,i*n-3*halo),min(i*n+n+2*halo,N))
-        i0,i1 = max(0,i*n-3*halo),min(i*n+n+2*halo,N)
-        #arr = [a.reshape(-1)[idx] for a in arrays]
-        #func(*arr, *args)
+        # idx = slice(max(0,i*n-3*halo),min(i*n+n+2*halo,N))
+        i0, i1 = max(0, i*n-3*halo), min(i*n+n+2*halo, N)
+        # arr = [a.reshape(-1)[idx] for a in arrays]
+        # func(*arr, *args)
         func(*arrays, *args, i0, i1)
