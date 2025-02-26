6a6f2a92a3d12cf85d5a4d51bd40131ffed8c7a4
pvthinker <roullet@univ-brest.fr>
2025-02-26 17:45:08 +0100

    Fix convection experiment

--- src/experiments/forced_convection.py
+++ src/experiments/forced_convection.py
@@ -4,15 +4,15 @@ import fluids2d as f2d
 
 def set_forcing(model, Q=5e-2, setup="RB", warmingfrac=0.5):
     model.mesh.time = model.time
-
+    nh = model.param.halowidth
     if setup == "RB":
         def forcing(param, mesh, s, ds):
-            ds.b[0] += Q
-            ds.b[1:-1] -= Q/(mesh.ny-1)
+            ds.b[nh, nh:-nh] += Q
+            ds.b[nh+1:-nh, nh:-nh] -= Q/(mesh.ny-1)
 
     elif setup == "HorizConv":
         imid = int(model.param.nx*warmingfrac)
-        jtop = -3
+        jtop = -1
         dy = model.mesh.dy
         Qcool = Q*warmingfrac/(1-warmingfrac)/dy
         Qwarm = Q/dy
@@ -20,8 +20,8 @@ def set_forcing(model, Q=5e-2, setup="RB", warmingfrac=0.5):
         def forcing(param, mesh, s, ds):
             t = mesh.time.t
             coef = np.tanh(t/50)
-            ds.b[jtop:-1, 0:imid] += Qwarm*coef
-            ds.b[jtop:-1, imid:-1] -= Qcool*coef
+            ds.b[jtop-nh:-nh, nh:imid] += Qwarm*coef
+            ds.b[jtop-nh:-nh, imid:-nh] -= Qcool*coef
 
     model.add_forcing(forcing)
 
@@ -44,24 +44,25 @@ if __name__ == "__main__":
 
     param.model = "boussinesq"
     param.Lx = 4
-    param.ny = 25
+    param.ny = 25*2
     param.nx = param.ny*param.Lx
     param.tend = 1_000
     param.maxite = 50_000
     param.cfl = 0.9
     param.dtmax = 1e-1
     param.xperiodic = False
+    param.noslip = ["bottom"]
 
     param.nplot = 10
     param.animation = True
     param.plotvar = "b"
-    param.clims = [-2, 10.]
+    # param.clims = [-10, 10.]
 
-    param.nhis = 10
+    param.nhis = 100
     param.var_to_store = ["b", "U"]
 
-   model = f2d.Model(param)
-   
+    model = f2d.Model(param)
+
     # set_forcing(model, setup="HorizConv", Q=0.1, warmingfrac=0.9)
     set_forcing(model, Q=1)
 
