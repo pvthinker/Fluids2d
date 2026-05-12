b522bc990771ee786df09c85efd864cda867d65d
groullet <guillaume.roullet@univ-brest.fr>
2026-05-12 09:02:21 +0200

    Store the forcing in model
    in the forced_convection experiment

--- src/experiments/forced_convection.py
+++ src/experiments/forced_convection.py
@@ -11,26 +11,31 @@ def set_forcing(model, Q=5e-2, setup="RB", warmingfrac=0.5):
             ds.b[nh+1:-nh, nh:-nh] -= Q/(mesh.ny-1)
 
     elif setup == "HorizConv":
-        imid = int(model.param.nx*warmingfrac)
+        nx = model.param.nx
+        imid = int(nx*warmingfrac)
         jtop = -1
         dy = model.mesh.dy
-        Qcool = Q*warmingfrac/(1-warmingfrac)/dy
+        Qcool = (Q/dy)*imid/(nx-imid)
         Qwarm = Q/dy
+        Qforc = model.state.b*0
+        Qforc[jtop-nh:-nh, nh:nh+imid] = Qwarm
+        Qforc[jtop-nh:-nh, nh+imid:-nh] = -Qcool
 
         def forcing(param, mesh, s, ds):
             t = mesh.time.t
-            coef = np.tanh(t/50)
-            ds.b[jtop-nh:-nh, nh:imid] += Qwarm*coef
-            ds.b[jtop-nh:-nh, imid:-nh] -= Qcool*coef
+            coef = 1#np.tanh(t*Q)
+            ds.b[jtop-nh:-nh, nh:nh+imid] += Qwarm*coef
+            ds.b[jtop-nh:-nh, nh+imid:-nh] -= Qcool*coef
 
     model.add_forcing(forcing)
+    model.Qforc = Qforc
 
 
 def set_initial_state(model):
     mesh = model.mesh
     x, y = mesh.xy()
     b = model.state.b
-    b[:, :] = 10*y+1e-2*np.random.normal(size=mesh.shape)
+    b[:, :] = 3*y+1e-2*np.random.normal(size=mesh.shape)
     b *= mesh.msk
 
 
@@ -40,31 +45,35 @@ if __name__ == "__main__":
 
     plt.ion()
 
+    Q = 0.8
+
     param = f2d.Param()
 
     param.model = "boussinesq"
     param.Lx = 4
     param.ny = 25*2
     param.nx = param.ny*param.Lx
-    param.tend = 1_000
-    param.maxite = 50_000
+    param.tend = 4/Q
+    param.maxite = 500_000
     param.cfl = 0.9
-    param.dtmax = 1e-1
+    param.dtmax = 1e-2/Q
     param.xperiodic = False
     param.noslip = ["bottom"]
+    param.integrator = "rk3"
 
-    param.nplot = 10
+    param.nplot = 200
     param.animation = True
     param.plotvar = "b"
     # param.clims = [-10, 10.]
 
-    param.nhis = 100
+    param.nhis = 10
     param.var_to_store = ["b", "U"]
 
     model = f2d.Model(param)
 
-    # set_forcing(model, setup="HorizConv", Q=0.1, warmingfrac=0.9)
-    set_forcing(model, Q=1)
+    np.random.seed(42)
+    set_forcing(model, setup="HorizConv", Q=Q, warmingfrac=0.95)
 
     set_initial_state(model)
-    model.run()
+    model.state.b[:,:] *= (10*Q)
+    #model.run()
